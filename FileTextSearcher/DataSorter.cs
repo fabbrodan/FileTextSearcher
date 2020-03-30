22507916896c951b736f03c0059eb508d2da34ce
Daniel Åslund <daniel.aaslund@gmail.com>
2020-03-30 13:38:34 +0200

    Added Descending/Ascending variants to quicksort

--- FileTextSearcher/DataSorter.cs
+++ FileTextSearcher/DataSorter.cs
@@ -38,25 +38,25 @@ namespace FileTextSearcher
             DataStruct = DataStruct.OrderByDescending(s => s).ToList();
         }
 
-        public void QuickSort()
+        public void QuickSortAscending()
         {
             int low = 0, high = DataStruct.Count - 1;
 
-            QuickSort(low, high);
+            QuickSortAscending(low, high);
         }
 
-        private void QuickSort(int low, int high)
+        private void QuickSortAscending(int low, int high)
         {
             if (low < high)
             {
-                int pi = Partition(low, high);
+                int pi = PartitionAscending(low, high);
 
-                QuickSort(low, pi - 1);
-                QuickSort(pi + 1, high);
+                QuickSortAscending(low, pi - 1);
+                QuickSortAscending(pi + 1, high);
             }
         }
 
-        private int Partition(int low, int high)
+        private int PartitionAscending(int low, int high)
         {
             T pivot = DataStruct[high];
             int i = low - 1;
@@ -65,6 +65,48 @@ namespace FileTextSearcher
             {
                 int comparison = DataStruct[j].CompareTo(pivot);
                 if (comparison < 0)
+                {
+                    i++;
+                    T temp = DataStruct[i];
+                    DataStruct[i] = DataStruct[j];
+                    DataStruct[j] = temp;
+                }
+            }
+
+            T temp2 = DataStruct[i + 1];
+            DataStruct[i + 1] = DataStruct[high];
+            DataStruct[high] = temp2;
+
+            return i + 1;
+        }
+
+        public void QuickSortDescending()
+        {
+            int low = 0, high = DataStruct.Count - 1;
+
+            QuickSortDescending(low, high);
+        }
+
+        private void QuickSortDescending(int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = PartitionDescending(low, high);
+
+                QuickSortDescending(low, pi - 1);
+                QuickSortDescending(pi + 1, high);
+            }
+        }
+
+        private int PartitionDescending(int low, int high)
+        {
+            T pivot = DataStruct[high];
+            int i = low - 1;
+
+            for (int j = low; j <= high - 1; j++)
+            {
+                int comparison = DataStruct[j].CompareTo(pivot);
+                if (comparison > 0)
                 {
                     i++;
                     T temp = DataStruct[i];
