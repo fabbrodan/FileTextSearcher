6035955c9d3a5913392a5365a5d5f6f83006dc56
David <david.l.larsson.90@gmail.com>
2020-03-31 19:24:59 +0200

    Added ReadFile class and add file function

--- FileTextSearcher/Form1.cs
+++ FileTextSearcher/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace FileTextSearcher
 {
     public partial class Form1 : Form
     {
+        private static List<ReadFile> readFiles = new List<ReadFile>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAddFiles_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                int counter = 0;
+                foreach (var item in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        string text = File.ReadAllText(item);
+                        ReadFile readFile = new ReadFile(openFileDialog.FileNames[counter], text);
+                        readFiles.Add(readFile);
+                    }
+                    catch (IOException)
+                    {
+                    }
+                    counter++;
+                }
+            }
         }
     }
 }
