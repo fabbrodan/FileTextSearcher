dcbe85a97745842b792d0f78134fb4254ec6e220
Martin <martin.goransson94@gmail.com>
2020-03-31 18:56:25 +0200

    Added save file dialog and temporary list creator

--- FileTextSearcher/Form1.Designer.cs
+++ FileTextSearcher/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.saveFileButton = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // saveFileButton
+            // 
+            this.saveFileButton.Location = new System.Drawing.Point(713, 415);
+            this.saveFileButton.Name = "saveFileButton";
+            this.saveFileButton.Size = new System.Drawing.Size(75, 23);
+            this.saveFileButton.TabIndex = 0;
+            this.saveFileButton.Text = "Save File";
+            this.saveFileButton.UseVisualStyleBackColor = true;
+            this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveFileButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
--- FileTextSearcher/Form1.cs
+++ FileTextSearcher/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,42 @@ namespace FileTextSearcher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void saveFileButton_Click(object sender, EventArgs e)
+        {
+            FileWriter fw = new FileWriter();
+            List<string> tempList = CreateNewList();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fullPath = saveFileDialog.FileName;
+                string fileName = Path.GetFileNameWithoutExtension(fullPath);
+                string fullDirectoryPath = Path.GetDirectoryName(fullPath);
+                fw.SaveFile(fullDirectoryPath, fileName, tempList);
+            }
+        }
+
+        public List<string> CreateNewList()
+        {
+            List<string> listToReturn = new List<string>();
+            Random rnd = new Random();
+            int numberOfWords = rnd.Next(1, 1000);
+            Random rndWord = new Random();
+            while (numberOfWords > 0)
+            {
+                int lengthOfTempWord = 20;
+                string tempStringToAddToList = "";
+                for (var i = 0; i < lengthOfTempWord; i++)
+                {
+                    tempStringToAddToList += ((char)(rndWord.Next(1, 26) + 64)).ToString();
+                }
+                listToReturn.Add(tempStringToAddToList);
+                numberOfWords--;
+            }
+            return listToReturn;
         }
     }
 }
