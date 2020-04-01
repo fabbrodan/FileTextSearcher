c4505a98a862384cf1fd604efc05d36606262be4
Hilda Olofsdotter <57358069+Olofsdotter@users.noreply.github.com>
2020-04-01 09:57:02 +0200

    search function

---
+++ SearchClassLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchClassLibrary
+{
+    public class SearchClass
+    {
+        public SearchClass()
+        {
+
+        }
+
+        //Method to convert string of text to an array of words
+        public string[] ConvertString(string file)
+        {
+            string[] text = file.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            return text;
+        }
+
+        //Using Linq to create a query to find the searched word within a text file. Input word can match captial letter and regular.
+        public int MatchOnSearchedWord(string[] text, string searchedWord)
+        {
+            var match = from word in text
+                        where (word.ToLowerInvariant() == searchedWord.ToLowerInvariant()) && (word.ToUpperInvariant() == searchedWord.ToUpperInvariant())
+                        select word;
+            int count = match.Count();
+            return count;
+
+        }
+    }
+}
