89024ea0ef005b58cc172482ac2a6cf8bf393384
Hilda Olofsdotter <57358069+Olofsdotter@users.noreply.github.com>
2020-04-01 10:17:25 +0200

    Added comments

--- SearchClassLibrary/Class1.cs
+++ SearchClassLibrary/Class1.cs
@@ -10,14 +10,25 @@ namespace SearchClassLibrary
 
         }
 
-        //Method to convert string of text to an array of words
+        
+        /// <summary>
+        /// Method to convert string of text to an array of words
+        /// </summary>
+        /// <param name="file">Text from text document</param>
+        /// <returns>String array containing extracted words from <paramref name="file"/></returns>
         public string[] ConvertString(string file)
         {
             string[] text = file.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
             return text;
         }
 
-        //Using Linq to create a query to find the searched word within a text file. Input word can match captial letter and regular.
+        
+        /// <summary>
+        /// Using Linq to create a query to find the searched word within a text file. Input word can match captial letter and regular
+        /// </summary>
+        /// <param name="text">String array of extracted words</param>
+        /// <param name="searchedWord">Searched word</param>
+        /// <returns>Count of matched <paramref name="searchedWord"/> in <paramref name="text"/></returns>
         public int MatchOnSearchedWord(string[] text, string searchedWord)
         {
             var match = from word in text
