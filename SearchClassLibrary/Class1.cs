6652e79361b379de0a010dc7ce7ec16458504b35
Hilda Olofsdotter <57358069+Olofsdotter@users.noreply.github.com>
2020-04-01 10:36:41 +0200

    Added search class

---
+++ SearchClassLibrary/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchClassLibrary
+{
+    public class SearchClass
+    {
+        public SearchClass()
+        {
+
+        }
+
+        
+        /// <summary>
+        /// Method to convert string of text to an array of words
+        /// </summary>
+        /// <param name="file">Text from text document</param>
+        /// <returns>String array containing extracted words from <paramref name="file"/></returns>
+        public string[] ConvertString(string file)
+        {
+            string[] text = file.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            return text;
+        }
+
+        
+        /// <summary>
+        /// Using Linq to create a query to find the searched word within a text file. Input word can match captial letter and regular
+        /// </summary>
+        /// <param name="text">String array of extracted words</param>
+        /// <param name="searchedWord">Searched word</param>
+        /// <returns>Count of matched <paramref name="searchedWord"/> in <paramref name="text"/></returns>
+        public int MatchOnSearchedWord(string[] text, string searchedWord)
+        {
+            var match = from word in text
+                        where (word.ToLowerInvariant() == searchedWord.ToLowerInvariant()) && (word.ToUpperInvariant() == searchedWord.ToUpperInvariant())
+                        select word;
+            int count = match.Count();
+            return count;
+
+        }
+    }
+}
